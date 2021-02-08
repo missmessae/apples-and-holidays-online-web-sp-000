a8882a038901a0c6aa5e66075527915c53ac631b
Annie Wiegel <annewiegel17@gmail.com>
2021-02-08 00:46:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
-    puts "#{season}: \n #{holiday}"
+    puts "#{season}: \n #{holiday}: \n"
 
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
